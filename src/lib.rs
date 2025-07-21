fa789f3fff79cbc9d471cecc5b5d1c6e475a13ef
bordi00 <matteo.bordignon@studenti.unitn.it>
2025-07-21 21:06:56 +0200

    feat: Added config parser and validator

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,48 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+mod errors;
+mod parser;
+mod utils;
 
 #[cfg(test)]
 mod tests {
+
+    // TODO: "Implement more test for parse and validation logic"
+
     use super::*;
+    use crate::parser::Parse;
+    use crate::parser::Validate;
+    use wg_internal::config::Config;
+    use crate::errors::ConfigError;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_parse_config() {
+        let config = Config::parse_config("./tests/correct_config.toml");
+        assert!(config.is_ok());
+    }
+
+    #[test]
+    fn test_validate_config() {
+        let config = Config::parse_config("./tests/correct_config.toml").unwrap();
+        let validation = config.validate_config();
+        assert!(validation.is_ok());
+    }
+
+    #[test]
+    fn test_unidirectional_error() {
+        let config = Config::parse_config("./tests/unidirectional_error.toml").unwrap();
+        let validation = config.validate_config();
+        assert_eq!(validation, Err(ConfigError::UnidirectedConnection));
+    }
+
+    #[test]
+    fn test_parsing_error() {
+        let config = Config::parse_config("./tests/invalid_config.toml");
+        assert!(config.is_err());
+    }
+
+    #[test]
+    fn test_invalid_node_connection() {
+        let config = Config::parse_config("./tests/invalid_node_connection1.toml").unwrap();
+        let validation = config.validate_config();
+        assert_eq!(validation, Err(ConfigError::InvalidNodeConnection("Drone 3 cannot be connected to itself".to_string())));
     }
 }
